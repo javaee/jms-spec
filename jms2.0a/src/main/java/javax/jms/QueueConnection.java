81026387520bab703b1566d35790e672babef4ad
Nigel Deakin <nigel.deakin@oracle.com>
2015-01-09 16:24:02 +0000

    Fix for JMS_SPEC-133: Update javadoc comments for QueueConnection#createQueueSession and TopicConnection#createTopicSession
    svn path=/; revision=316

--- jms2.0a/src/main/java/javax/jms/QueueConnection.java
+++ jms2.0a/src/main/java/javax/jms/QueueConnection.java
@@ -73,27 +73,100 @@ package javax.jms;
 
 public interface QueueConnection extends Connection {
 
-    /** Creates a {@code QueueSession} object.
-      *  
-      * @param transacted indicates whether the session is transacted
-      * @param acknowledgeMode indicates whether the consumer or the
-      * client will acknowledge any messages it receives; ignored if the session
-      * is transacted. Legal values are {@code Session.AUTO_ACKNOWLEDGE}, 
-      * {@code Session.CLIENT_ACKNOWLEDGE}, and 
-      * {@code Session.DUPS_OK_ACKNOWLEDGE}.
-      *  
-      * @return a newly created queue session
-      *  
-      * @exception JMSException if the {@code QueueConnection} object fails
-      *                         to create a session due to some internal error or
-      *                         lack of support for the specific transaction
-      *                         and acknowledgement mode.
-      *
-      * @see Session#AUTO_ACKNOWLEDGE 
-      * @see Session#CLIENT_ACKNOWLEDGE 
-      * @see Session#DUPS_OK_ACKNOWLEDGE 
-      */ 
-
+    /** 
+     * Creates a {@code QueueSession} object, 
+     * specifying {@code transacted} and {@code acknowledgeMode}.
+     * <p> 
+     * The effect of setting the {@code transacted} and {@code acknowledgeMode} 
+     * arguments depends on whether this method is called in a Java SE environment, 
+     * in the Java EE application client container, or in the Java EE web or EJB container.
+     * If this method is called in the Java EE web or EJB container then the 
+     * effect of setting the transacted} and {@code acknowledgeMode} 
+     * arguments also depends on whether or not there is an active JTA transaction 
+     * in progress.  
+     * <p>
+     * In a <b>Java SE environment</b> or in <b>the Java EE application client container</b>:
+     * <ul>
+     * <li>If {@code transacted} is set to {@code true} then the session 
+     * will use a local transaction which may subsequently be committed or rolled back 
+     * by calling the session's {@code commit} or {@code rollback} methods. 
+     * The argument {@code acknowledgeMode} is ignored.
+     * <li>If {@code transacted} is set to {@code false} then the session 
+     * will be non-transacted. In this case the argument {@code acknowledgeMode}
+     * is used to specify how messages received by this session will be acknowledged.
+     * The permitted values are 
+     * {@code Session.CLIENT_ACKNOWLEDGE}, 
+     * {@code Session.AUTO_ACKNOWLEDGE} and
+     * {@code Session.DUPS_OK_ACKNOWLEDGE}.
+     * For a definition of the meaning of these acknowledgement modes see the links below.
+     * </ul>
+     * <p>
+     * In a <b>Java EE web or EJB container, when there is an active JTA transaction in progress</b>:
+     * <ul>
+     * <li>Both arguments {@code transacted} and {@code acknowledgeMode} are ignored.
+     * The session will participate in the JTA transaction and will be committed or rolled back
+     * when that transaction is committed or rolled back, 
+     * not by calling the session's {@code commit} or {@code rollback} methods.
+     * Since both arguments are ignored, developers are recommended to use 
+     * {@code createSession()}, which has no arguments, instead of this method.
+     * </ul>
+     * <p>
+     * In the <b>Java EE web or EJB container, when there is no active JTA transaction in progress</b>:
+     * <ul>
+     * <li>
+     * If {@code transacted} is set to false and {@code acknowledgeMode} is set to
+     * {@code JMSContext.AUTO_ACKNOWLEDGE} or {@code Session.DUPS_OK_ACKNOWLEDGE} then the
+     * session will be non-transacted and messages will be acknowledged according
+     * to the value of {@code acknowledgeMode}.
+     * <li>
+     * If {@code transacted} is set to false and {@code acknowledgeMode} is set to
+     * {@code JMSContext.CLIENT_ACKNOWLEDGE} then the JMS provider is recommended to
+     * ignore the specified parameters and instead provide a non-transacted,
+     * auto-acknowledged session. However the JMS provider may alternatively
+     * provide a non-transacted session with client acknowledgement.
+     * <li>
+     * If {@code transacted} is set to true, then the JMS provider is recommended to
+     * ignore the specified parameters and instead provide a non-transacted,
+     * auto-acknowledged session. However the JMS provider may alternatively
+     * provide a local transacted session.
+     * <li>
+     * Applications are recommended to set {@code transacted} to false and
+     * {@code acknowledgeMode} to {@code JMSContext.AUTO_ACKNOWLEDGE} or
+     * {@code Session.DUPS_OK_ACKNOWLEDGE} since since applications which set
+     * {@code transacted} to false and set {@code acknowledgeMode} to
+     * {@code JMSContext.CLIENT_ACKNOWLEDGE}, or which set {@code transacted} 
+     * to true, may not be portable. 
+     * </ul> 
+     * <p>
+     * Applications running in the Java EE web and EJB containers must not attempt 
+     * to create more than one active (not closed) {@code Session} object per connection. 
+     * If this method is called in a Java EE web or EJB container when an active
+     * {@code Session} object already exists for this connection then a {@code JMSException} may be thrown.
+     * 
+     * @param transacted indicates whether the session will use a local transaction,
+     * except in the cases described above when this value is ignored.
+     * 
+     * @param acknowledgeMode when transacted is false, indicates how messages received
+     * by the session will be acknowledged, except in the cases described above
+     * when this value is ignored.
+     * 
+     * @return a newly created {@code QueueSession}
+     *  
+     * @exception JMSException if the {@code QueueConnection} object fails
+     *                         to create a {@code QueueSession} due to 
+     *                         <ul>
+     *                         <li>some internal error, 
+     *                         <li>lack of support for the specific transaction and acknowledgement mode, or
+     *                         <li>because this method is being called in a Java EE web or EJB application 
+     *                         and an active session already exists for this connection.
+     *                         </ul>
+     * @since JMS 1.1
+     *
+     * @see Session#AUTO_ACKNOWLEDGE 
+     * @see Session#CLIENT_ACKNOWLEDGE 
+     * @see Session#DUPS_OK_ACKNOWLEDGE 
+     * 
+     */ 
     QueueSession
     createQueueSession(boolean transacted,
                        int acknowledgeMode) throws JMSException;
